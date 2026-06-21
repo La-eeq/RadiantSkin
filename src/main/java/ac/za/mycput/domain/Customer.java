b0833b014c9ab583502698af7938b2cdf77c0828
tebogomakgato26 <230116086@mycput.ac.za>
2026-06-21 14:49:45 +0200

    Updated Customer entity

--- src/main/java/ac/za/mycput/domain/Customer.java
+++ src/main/java/ac/za/mycput/domain/Customer.java
@@ -1,4 +1,28 @@
 package ac.za.mycput.domain;
 
-public class Customer {
+import jakarta.persistence.Entity;
+
+@Entity
+public class Customer extends User {
+
+    private String phoneNumber;
+
+    protected Customer() {
+
+    }
+
+    public Customer(Long userId, String firstName, String lastName, String email,
+                    String password, String phoneNumber) {
+        super(userId, firstName, lastName, email, password);
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getPhoneNumber() { return phoneNumber; }
+
+    @Override
+    public String toString() {
+        return "Customer{" +
+                "phoneNumber='" + phoneNumber + '\'' +
+                "} " + super.toString();
+    }
 }
